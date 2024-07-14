ca5dffbffa3e3d626262e9bc45aa2f541e818545
Muhammad Tanveer <tanveerm176@gmail.com>
2024-07-14 00:52:16 -0400

    add error and loading screen icons and inc text size

--- app/src/main/java/com/example/fetchandroidexercise/ui/screens/ErrorScreen.kt
+++ app/src/main/java/com/example/fetchandroidexercise/ui/screens/ErrorScreen.kt
@@ -1,11 +1,20 @@
 package com.example.fetchandroidexercise.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.fetchandroidexercise.R
 
 /**
@@ -15,12 +24,21 @@ import com.example.fetchandroidexercise.R
  */
 @Composable
 fun ErrorScreen(modifier : Modifier = Modifier) {
-    Box(
-        contentAlignment = Alignment.Center,
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
     ) {
+        Icon(
+            imageVector = Icons.Default.Warning,
+            contentDescription = "Loading Icon",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(50.dp)
+        )
         Text(
-            text = stringResource(R.string.error_text)
+            text = stringResource(R.string.error_text),
+            fontSize = 28.sp
         )
     }
 }
--- app/src/main/java/com/example/fetchandroidexercise/ui/screens/LoadingScreen.kt
+++ app/src/main/java/com/example/fetchandroidexercise/ui/screens/LoadingScreen.kt
@@ -1,12 +1,26 @@
 package com.example.fetchandroidexercise.ui.screens
 
+import android.widget.Space
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.fetchandroidexercise.R
+import com.example.fetchandroidexercise.ui.FetchUiState
+import com.example.fetchandroidexercise.ui.theme.FetchAndroidExerciseTheme
 
 /**
  * Composable function representing the loading screen.
@@ -14,13 +28,31 @@ import com.example.fetchandroidexercise.R
  * @param modifier Optional modifier for adjusting layout and styling.
  */
 @Composable
-fun LoadingScreen(modifier : Modifier = Modifier) {
-    Box(
-        contentAlignment = Alignment.Center,
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
+
     ) {
-        Text(
-            text = stringResource(R.string.loading_data_text)
+        Icon(
+            imageVector = Icons.Default.Refresh,
+            contentDescription = "Loading Icon",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(50.dp)
         )
+        Text(
+            text = stringResource(R.string.loading_data_text),
+            fontSize = 28.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLoadingScreen() {
+    FetchAndroidExerciseTheme {
+        LoadingScreen()
     }
 }
