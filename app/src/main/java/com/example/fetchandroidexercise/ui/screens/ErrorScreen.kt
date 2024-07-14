a653ccd48560b9f20a0ce0be2e3eef630c98f958
Muhammad Tanveer <tanveerm176@gmail.com>
2024-07-14 01:16:59 -0400

    added error screen icon

--- app/src/main/java/com/example/fetchandroidexercise/ui/screens/ErrorScreen.kt
+++ app/src/main/java/com/example/fetchandroidexercise/ui/screens/ErrorScreen.kt
@@ -1,21 +1,26 @@
 package com.example.fetchandroidexercise.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.fetchandroidexercise.R
+import com.example.fetchandroidexercise.data.FetchItemsRepository
+import com.example.fetchandroidexercise.ui.FetchUiState
+import com.example.fetchandroidexercise.ui.FetchViewModel
+import com.example.fetchandroidexercise.ui.theme.FetchAndroidExerciseTheme
 
 /**
  * Composable function representing the error screen.
@@ -23,7 +28,7 @@ import com.example.fetchandroidexercise.R
  * @param modifier Optional modifier for adjusting layout and styling.
  */
 @Composable
-fun ErrorScreen(modifier : Modifier = Modifier) {
+fun ErrorScreen(modifier : Modifier = Modifier/*, fetchUiState: FetchUiState*/) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -38,7 +43,23 @@ fun ErrorScreen(modifier : Modifier = Modifier) {
         )
         Text(
             text = stringResource(R.string.error_text),
-            fontSize = 28.sp
+            fontSize = 28.sp,
+            modifier = Modifier.padding(vertical = 10.dp)
         )
+
+        /*Button(
+            onClick = { *//*TO DO*//* },
+            modifier = Modifier.padding(vertical = 5.dp)
+        ) {
+            Text(text = "Refresh")
+        }*/
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorScreenPreview(){
+    FetchAndroidExerciseTheme {
+        ErrorScreen()
     }
 }
